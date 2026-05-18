681990af25d46fd70d2fd302bfb940240c5c9a60
Carlos Verdugo <carlos.versa99@gmail.com>
2026-05-18 12:43:50 -0700

    Initial commit

---
+++ src/test/java/com/carlos/landon_hotel/LandonHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.landon_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandonHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
